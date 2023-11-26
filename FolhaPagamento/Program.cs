18d731c60a610e2388935e47d05ba1ff938c594e
Eduardo Vieira <eduardoeoq7@gmail.com>
2023-11-25 23:20:25 -0300

    Commit test

--- FolhaPagamento/Program.cs
+++ FolhaPagamento/Program.cs
@@ -11,7 +11,6 @@ namespace FolhaPagamento
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration. 
             Application.Run(new Tela_Login());
-
         }
     }
 }
